296942fe21e339ff515ebd8e45cab71f1754eafc
Ricardo Gonçalves <ricardo@mytiki.com>
2022-02-02 23:51:56 -0300

    pin recover loading

--- lib/src/modal_recover/ui/modal_recover_ui_view_pin.dart
+++ lib/src/modal_recover/ui/modal_recover_ui_view_pin.dart
@@ -5,6 +5,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:lottie/lottie.dart';
 import 'package:provider/provider.dart';
 
 import '../modal_recover_service.dart';
@@ -16,23 +17,30 @@ abstract class ModalRecoverUiViewPin extends ModalRecoverUiView {
   @override
   Widget page(BuildContext context) {
     ModalRecoverService service = Provider.of<ModalRecoverService>(context);
-    return Container(
-        height: MediaQuery.of(context).viewInsets.bottom + style.size(200),
-        padding: EdgeInsets.all(style.modalContentPadding),
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            mainAxisSize: MainAxisSize.max,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              ModalRecoverWidgetText(title),
-              Container(
-                  padding: EdgeInsets.only(bottom: style.size(50)),
-                  child: service.state.error == null
-                      ? ModalRecoverWidgetText(subtitle)
-                      : ModalRecoverWidgetText(service.state.error!,
-                          color: style.errorColor)),
-              ModalRecoverWidgetPin((pin) => onSubmit(context, pin)),
-            ]));
+    return Stack(children: [
+              Padding(
+              padding: EdgeInsets.all(style.modalContentPadding),
+              child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                ModalRecoverWidgetText(title),
+                Container(
+                    padding: EdgeInsets.only(bottom: style.size(50)),
+                    child: service.state.error == null
+                        ? ModalRecoverWidgetText(subtitle)
+                        : ModalRecoverWidgetText(service.state.error!,
+                            color: style.errorColor)),
+                ModalRecoverWidgetPin((pin) => onSubmit(context, pin)),
+              ])),
+              if (service.state.loading)
+                Container(
+                  color: Color.fromRGBO(255, 255, 255, 0.5),
+                  child: Align(
+                      alignment: Alignment.topCenter,
+                      child: Lottie.asset("res/animation/recover-loading.json",
+                          package: 'login', height: style.size(100))))]);
   }
 
   String get title;
